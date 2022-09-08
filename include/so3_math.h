5c7ca90d9843957666288d8a1acd68894673f060
zlwang <57713545+zlwang7@users.noreply.github.com>
2022-09-08 12:02:51 +0800

    Add files via upload

--- include/so3_math.h
+++ include/so3_math.h
@@ -4,10 +4,15 @@
 #include <math.h>
 #include <Eigen/Core>
 
-#define SKEW_SYM_MATRX(v) 0.0,-v[2],v[1],v[2],0.0,-v[0],-v[1],v[0],0.0
+//  Annotated by zlwang  2022.9
+/*
+这个.h主要包含：指数映射的函数
+*/
+
+#define SKEW_SYM_MATRX(v) 0.0,-v[2],v[1],v[2],0.0,-v[0],-v[1],v[0],0.0   //李代数对应的反对称矩阵  fai^
 
 template<typename T>
-Eigen::Matrix<T, 3, 3> skew_sym_mat(const Eigen::Matrix<T, 3, 1> &v)
+Eigen::Matrix<T, 3, 3> skew_sym_mat(const Eigen::Matrix<T, 3, 1> &v)   //没用到
 {
     Eigen::Matrix<T, 3, 3> skew_sym_mat;
     skew_sym_mat<<0.0,-v[2],v[1],v[2],0.0,-v[0],-v[1],v[0],0.0;
@@ -15,26 +20,26 @@ Eigen::Matrix<T, 3, 3> skew_sym_mat(const Eigen::Matrix<T, 3, 1> &v)
 }
 
 template<typename T>
-Eigen::Matrix<T, 3, 3> Exp(const Eigen::Matrix<T, 3, 1> &&ang)
+Eigen::Matrix<T, 3, 3> Exp(const Eigen::Matrix<T, 3, 1> &&ang)     //指数映射：从转角，李代数(3X1)  到  旋转矩阵，李群(3X3)
 {
     T ang_norm = ang.norm();
     Eigen::Matrix<T, 3, 3> Eye3 = Eigen::Matrix<T, 3, 3>::Identity();
     if (ang_norm > 0.0000001)
     {
-        Eigen::Matrix<T, 3, 1> r_axis = ang / ang_norm;
+        Eigen::Matrix<T, 3, 1> r_axis = ang / ang_norm;   //对应公式 fai = theta * a 中的a, ang_norm对应theta
         Eigen::Matrix<T, 3, 3> K;
         K << SKEW_SYM_MATRX(r_axis);
-        /// Roderigous Tranformation
+        /// Roderigous Tranformation    指数映射
         return Eye3 + std::sin(ang_norm) * K + (1.0 - std::cos(ang_norm)) * K * K;
     }
-    else
+    else    //如果角度变换太小  直接赋值为I
     {
         return Eye3;
     }
 }
 
 template<typename T, typename Ts>
-Eigen::Matrix<T, 3, 3> Exp(const Eigen::Matrix<T, 3, 1> &ang_vel, const Ts &dt)
+Eigen::Matrix<T, 3, 3> Exp(const Eigen::Matrix<T, 3, 1> &ang_vel, const Ts &dt)   //指数映射：同理，表示Exp(角速度*dt)
 {
     T ang_vel_norm = ang_vel.norm();
     Eigen::Matrix<T, 3, 3> Eye3 = Eigen::Matrix<T, 3, 3>::Identity();
@@ -58,7 +63,7 @@ Eigen::Matrix<T, 3, 3> Exp(const Eigen::Matrix<T, 3, 1> &ang_vel, const Ts &dt)
 }
 
 template<typename T>
-Eigen::Matrix<T, 3, 3> Exp(const T &v1, const T &v2, const T &v3)
+Eigen::Matrix<T, 3, 3> Exp(const T &v1, const T &v2, const T &v3)    //指数映射  但没用到
 {
     T &&norm = sqrt(v1 * v1 + v2 * v2 + v3 * v3);
     Eigen::Matrix<T, 3, 3> Eye3 = Eigen::Matrix<T, 3, 3>::Identity();
@@ -79,15 +84,15 @@ Eigen::Matrix<T, 3, 3> Exp(const T &v1, const T &v2, const T &v3)
 
 /* Logrithm of a Rotation Matrix */
 template<typename T>
-Eigen::Matrix<T,3,1> Log(const Eigen::Matrix<T, 3, 3> &R)
+Eigen::Matrix<T,3,1> Log(const Eigen::Matrix<T, 3, 3> &R)    //对数映射  从旋转矩阵，李群(3X3)   到   转角，李代数(3X1)      
 {
-    T theta = (R.trace() > 3.0 - 1e-6) ? 0.0 : std::acos(0.5 * (R.trace() - 1));
-    Eigen::Matrix<T,3,1> K(R(2,1) - R(1,2), R(0,2) - R(2,0), R(1,0) - R(0,1));
-    return (std::abs(theta) < 0.001) ? (0.5 * K) : (0.5 * theta / std::sin(theta) * K);
+    T theta = (R.trace() > 3.0 - 1e-6) ? 0.0 : std::acos(0.5 * (R.trace() - 1));   //求theta值  如果矩阵R的迹接近3 说明矩阵接近单位阵直接令theta=0
+    Eigen::Matrix<T,3,1> K(R(2,1) - R(1,2), R(0,2) - R(2,0), R(1,0) - R(0,1));   //对应 (R-R^T)V
+    return (std::abs(theta) < 0.001) ? (0.5 * K) : (0.5 * theta / std::sin(theta) * K);   
 }
 
 template<typename T>
-Eigen::Matrix<T, 3, 1> RotMtoEuler(const Eigen::Matrix<T, 3, 3> &rot)
+Eigen::Matrix<T, 3, 1> RotMtoEuler(const Eigen::Matrix<T, 3, 3> &rot)    //旋转矩阵转欧拉角（没用到）
 {
     T sy = sqrt(rot(0,0)*rot(0,0) + rot(1,0)*rot(1,0));
     bool singular = sy < 1e-6;
